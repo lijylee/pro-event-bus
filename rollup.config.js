fb81973458e3d8227e8ad0c789216350e444de53
lijy <lijylee@126.com>
2022-03-11 15:06:02 +0800

    chore: update dependencies && uglify && format

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,33 @@
 import resolve from '@rollup/plugin-node-resolve'
 import babel from '@rollup/plugin-babel'
+import { uglify } from "rollup-plugin-uglify"
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'pro-event-bus/index.js',
-    format: 'umd',
-    name: 'ProEventBus'
-  },
+  output: [
+    {
+      file: 'pro-event-bus/index.js',
+      format: 'umd',
+      name: 'ProEventBus'
+    },
+    {
+      file: 'pro-event-bus/index-web.js',
+      format: 'iife',
+      name: 'ProEventBus'
+    },
+    {
+      file: 'pro-event-bus/index-cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'pro-event-bus/index.js',
+      format: 'amd'
+    },
+    {
+      file: 'pro-event-bus/index-es.js',
+      format: 'esm'
+    },
+  ],
   plugins: [
     resolve(),
     babel(
@@ -15,6 +35,7 @@ export default {
         exclude: '**/node_modules/**',
         babelHelpers: 'bundled'
       }
-    )
+    ),
+    uglify()
   ]
 }
